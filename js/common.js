e8e915e9f0ea9c9cdd7f742a2401e715b2dfa0d4
ladytwinty <ladytwinty@gmail.com>
2017-03-31 17:07:43 +0300

    edited

--- js/common.js
+++ js/common.js
@@ -5,24 +5,8 @@ $(".menu_wrap li").click(function(){
 		$(this).addClass("active");
 	});
 
-	$(".bxslider").bxSlider();
-
-	$(".slider2").bxSlider({
-    slideWidth: 180,
-    captions: true,
-    minSlides: 4,
-    maxSlides: 4,
-    moveSlides: 1,
-    slideMargin: 20
-  });
-
-	$(".slider3").bxSlider({
-    slideWidth: 180,
-    captions: true,
-    minSlides: 4,
-    maxSlides: 8,
-    moveSlides: 1,
-    slideMargin: 20
-  });
+	$(".bxslider").bxSlider({
+        captions: true
+    });
 
 });
